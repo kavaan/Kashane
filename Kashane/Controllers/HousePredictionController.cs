8c89a2a4fef64e9158402f6d0cbfda9ec0842064
keivan <kavaandamirchi@gmail.com>
2024-12-17 02:41:35 +0330

    change loading algo

--- Kashane/Controllers/HousePredictionController.cs
+++ Kashane/Controllers/HousePredictionController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.ML;
+using Microsoft.ML.Data;
 using HousePricePredictionAPI.Models;
 using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.IO;
 using HousePricePredictionAPI.Models.HousePricePredictionAPI.Models;
+using Microsoft.ML.Trainers.FastTree;
+
 
 namespace HousePricePredictionAPI.Controllers
 {
@@ -15,17 +20,20 @@ namespace HousePricePredictionAPI.Controllers
         public HousePredictionController(IWebHostEnvironment env)
         {
             var mlContext = new MLContext();
+            var model = TrainModel(mlContext, env);
+            _predictionEngine = mlContext.Model.CreatePredictionEngine<HouseData, HousePricePrediction>(model);
+        }
 
-            // مسیر فایل در wwwroot/Data
+        private ITransformer TrainModel(MLContext mlContext, IWebHostEnvironment env)
+        {
             string modelPath = Path.Combine(env.WebRootPath, "Data", "updated_house_data_babol.csv");
 
-            // Load data and train the model
+            // بارگذاری داده‌ها
             IDataView dataView = mlContext.Data.LoadFromTextFile<HouseData>(modelPath, separatorChar: ',', hasHeader: true);
 
+            // پیش‌پردازش داده‌ها
             var pipeline = mlContext.Transforms.CopyColumns("Label", "Price")
                 .Append(mlContext.Transforms.Categorical.OneHotEncoding("Neighborhood"))
-                .Append(mlContext.Transforms.Categorical.OneHotEncoding("Elevator"))
-                .Append(mlContext.Transforms.Categorical.OneHotEncoding("Parking"))
                 .Append(mlContext.Transforms.Categorical.OneHotEncoding("PropertyType"))
                 .Append(mlContext.Transforms.Categorical.OneHotEncoding("DocumentType"))
                 .Append(mlContext.Transforms.Categorical.OneHotEncoding("NearCenters"))
@@ -33,17 +41,19 @@ namespace HousePricePredictionAPI.Controllers
                 .Append(mlContext.Transforms.Categorical.OneHotEncoding("FlooringType"))
                 .Append(mlContext.Transforms.Categorical.OneHotEncoding("FacadeType"))
                 .Append(mlContext.Transforms.Categorical.OneHotEncoding("ExtraAmenities"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding("Elevator"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding("Parking"))
                 .Append(mlContext.Transforms.Concatenate("Features", new[]
                 {
                     "Neighborhood", "Area", "Rooms", "YearBuilt", "Floors", "FloorNumber",
                     "Elevator", "Parking", "PropertyType", "DocumentType", "NearCenters",
                     "RenovationStatus", "FlooringType", "FacadeType", "ExtraAmenities"
                 }))
-                .Append(mlContext.Regression.Trainers.LbfgsPoissonRegression());
+                .Append(mlContext.Regression.Trainers.FastTree());
 
+            // آموزش مدل
             var model = pipeline.Fit(dataView);
-
-            _predictionEngine = mlContext.Model.CreatePredictionEngine<HouseData, HousePricePrediction>(model);
+            return model;
         }
 
         [HttpPost]
@@ -53,7 +63,12 @@ namespace HousePricePredictionAPI.Controllers
         )]
         public ActionResult<float> Predict([FromBody] HouseInputData houseInput)
         {
-            // تبدیل HouseInputData به HouseData بدون Price
+            var adjustedPrice = AdjustPrice(houseInput);
+            return Ok(new { PredictedPrice = $"{adjustedPrice:N0} تومان" });
+        }
+
+        private float AdjustPrice(HouseInputData houseInput)
+        {
             var houseData = new HouseData
             {
                 Neighborhood = houseInput.Neighborhood,
@@ -74,11 +89,48 @@ namespace HousePricePredictionAPI.Controllers
             };
 
             var prediction = _predictionEngine.Predict(houseData);
+            float finalPrice = prediction.Price;
 
-            // قالب‌بندی قیمت به صورت سه رقم سه رقم و افزودن کلمه "تومان"
-            string formattedPrice = $"{prediction.Price:N0} تومان";
+            // تنظیم قیمت بر اساس ویژگی‌های مختلف
 
-            return Ok(new { PredictedPrice = formattedPrice });
+            // تاثیر آسانسور
+            if (houseInput.Elevator == "Yes")
+                finalPrice *= 1.05f;
+
+            // تاثیر پارکینگ
+            if (houseInput.Parking == "Yes")
+                finalPrice *= 1.10f;
+
+            // تاثیر مکان‌های نزدیک
+            switch (houseInput.NearCenters)
+            {
+                case "School":
+                    finalPrice *= 0.95f;
+                    break;
+                case "MainStreet":
+                    finalPrice *= 1.15f;
+                    break;
+                case "ShoppingCenter":
+                case "Park":
+                    finalPrice *= 1.10f;
+                    break;
+            }
+
+            // تاثیر نوع نما
+            switch (houseInput.FacadeType)
+            {
+                case "Brick":
+                    finalPrice *= 0.90f;
+                    break;
+                case "Cement":
+                    finalPrice *= 0.92f;
+                    break;
+                case "Stone":
+                    finalPrice *= 1.08f;
+                    break;
+            }
+
+            return finalPrice;
         }
     }
 }
